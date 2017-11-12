f46ce6937e1a1c70a82a8ca788c5e01c7ac43f08
ArendJanH <arendjan.hengst@gmail.com>
2017-11-12 17:57:37 +0100

    Updated Core #7 #3 #6 #5 #4
    Added the code for the SD card and altitude sensor. Also added some more comments

--- Arduino/Core/Core.ino
+++ Arduino/Core/Core.ino
@@ -1,5 +1,6 @@
 // Load essential Libraries
 #include <Wire.h>           // Load liberary needed for I2C
+
 // temporary because of weird out-of-scope errors
 float pitch, roll, heading;
 // altitude sensor variables
@@ -23,6 +24,10 @@ void setup() {
   // Altitude Sensor //
   /////////////////////
   Altitude_Sensor();
+  /////////////////////
+  //     SD Card     //
+  /////////////////////
+  SD_Card();
 }
 
 void loop() {
@@ -32,6 +37,7 @@ void loop() {
   //Serial.println("Heading: "); 
   //Serial.println(rotatie_meter_heading());
 
+  // output rotation data
   rotationSensorCalcOrientation();
   Serial.print("Pitch, Roll: ");
   Serial.print(pitch, 2);
@@ -39,13 +45,20 @@ void loop() {
   Serial.println(roll, 2);
   Serial.print("Heading: "); 
   Serial.println(heading, 2);
-  
+
+  // output temperature and altitude data
   altitudeCalc();
   temperatureCalc();
   Serial.print("altitude: "); 
   Serial.println(altitude, 2);
   Serial.print("temperature: "); 
   Serial.println(temperature, 2);
+
+  // save to SD card
+  sdCardOpenFile();
+  sdCardPrint(temperature);
+  sdCardCloseFile();
+  
   delay(1000);
 
   
